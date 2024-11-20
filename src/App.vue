e7638b335a1b09607fe804b50670d1b6d363a3ae
Kim-jong-yeop <114080381+kjy0124@users.noreply.github.com>
2024-11-20 22:41:58 +0900

    ******commit먼저부탁하고 작업ㄱㄱㄱ*******

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div id="app">
     <UserLogin />
   </div>
@@ -7,34 +6,11 @@
 
 <script>
 import UserLogin from './components/UserLogin.vue';
-=======
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
->>>>>>> 4097b5f (Update test.txt and blogproject)
 
 export default {
   name: 'App',
   components: {
-<<<<<<< HEAD
     UserLogin,
-=======
-    HelloWorld
->>>>>>> 4097b5f (Update test.txt and blogproject)
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,6 +1,14 @@
 <template lang="ko">
   <div class="container">
-    <h1>hello</h1>
+    <h1>BlogProject</h1>
+    <div class="login-container">
+      <h2>Login</h2>
+      <form method="post" action="서버의 url" id="login-form">
+        <input type="text" name="userId" placeholder="아이디를 입력하세요.">
+        <input type="password" name="userPassword" placeholder="비밀번호를 입력하세요.">
+
+      </form>
+    </div>
   </div>
 </template>
 <script>
@@ -8,6 +16,13 @@ export default {
   name: 'UserLogin',
 }
 </script>
-<style lang="">
-  
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+    border: none;
+  }
+  body{
+    font-size: 14px;
+  }
 </style>
