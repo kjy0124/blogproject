0599578bb8a9173ca7152bef823bbefb3c1c20e0
ahnbyeongwoo <bw5871@gmail.com>
2024-11-21 17:26:49 +0900

    test.txt

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <UserLogin />
+  </div>
+</template>
+
+<script>
+import UserLogin from './components/UserLogin.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UserLogin,
+  }
+}
+</script>
